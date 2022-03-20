0730ce35052d63e2082f2c397b340bab5b14412c
Lukas <lukas.kliemank@googlemail.com>
2022-03-20 12:15:37 +0100

    initial commit

---
+++ tags-im-museum/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navbar, NavDropdown, Nav, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">Tags im Museum - Tagebücher</Navbar.Brand>
+            {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse> */}
+          </Container>
+        </Navbar>
+      </header>
+      <main>
+      </main>
+      <footer>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
